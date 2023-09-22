741f3b073b567ea8bc1ace6667a0a71bec2e1ddb
Sofia Escobedo <cdmx.sofia@gmail.com>
2023-09-22 12:03:09 -0600

    update frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
-import {Title} from '@angular/platform-browser';
-import {Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
-import {Auth} from 'aws-amplify';
+import { Title } from '@angular/platform-browser';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Amplify, Auth } from 'aws-amplify';
 import { AuthenticatorService } from '@aws-amplify/ui-angular';
+import awsExports from '../aws-exports';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +16,13 @@ import { AuthenticatorService } from '@aws-amplify/ui-angular';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  @ViewChild("main") main: ElementRef | undefined;
-  @ViewChild("footer") footer: ElementRef | undefined;
-  constructor(private titleService: Title,public authenticator: AuthenticatorService) {
-    this.titleService.setTitle($localize`Portfolio`);
-  }
+  @ViewChild('main') main: ElementRef | undefined;
+  @ViewChild('footer') footer: ElementRef | undefined;
 
-
-  services = {
+  services: any = {
     async handleSignUp(formData: Record<string, any>) {
-      // eslint-disable-next-line prefer-const
-      let { username, password, attributes } = formData;
-      console.log(attributes)
-      // custom username
-      // username = username.toLowerCase();
-      // attributes.email = attributes.email.toLowerCase();
+      const { username, password, attributes } = formData;
+      attributes.picture = 'default-profile-picture.jpg';
       return Auth.signUp({
         username,
         password,
@@ -33,6 +31,14 @@ export class AppComponent {
           enabled: true,
         },
       });
-    }
+    },
+  };
+
+  constructor(
+    private titleService: Title,
+    public authenticator: AuthenticatorService
+  ) {
+    Amplify.configure(awsExports);
+    this.titleService.setTitle($localize`Portfolio`);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {RouterModule} from '@angular/router';
-import {appRoutes} from './app.routes';
-import {RomaScriptComponent} from './roma-script/roma-script.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
+import { RomaScriptComponent } from './roma-script/roma-script.component';
 
-import {AmplifyAuthenticatorModule} from '@aws-amplify/ui-angular';
-import {AppRoutingModule} from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatCardModule} from '@angular/material/card';
-import {MarkdownModule} from 'ngx-markdown';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatInputModule} from '@angular/material/input';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MarkdownModule } from 'ngx-markdown';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, RomaScriptComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
@@ -30,9 +31,10 @@ import {MatInputModule} from '@angular/material/input';
     MatFormFieldModule,
     MatIconModule,
     MatRadioModule,
-    MatInputModule
+    MatInputModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
