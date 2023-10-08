b6ad6dbdb54e0cf3a80e1be083afe1be348a3982
Sofia Escobedo <cdmx.sofia@gmail.com>
2023-10-07 18:13:44 -0600

    removed YouTube search and markdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { YouTubePlayerModule } from '@angular/youtube-player';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatCardModule } from '@angular/material/card';
-import { MarkdownModule } from 'ngx-markdown';
-import { MatIconModule } from '@angular/material/icon';
+
 import { MatRadioModule } from '@angular/material/radio';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AppComponent } from './app.component';
 import { Error404Component } from './error404/error404.component';
+
 import { NoicefluidComponent } from './noicefluid/noicefluid.component';
 import { YtPlayerComponent } from './noicefluid/yt-player/yt-player.component';
 import { ChatComponent } from './noicefluid/chat/chat.component';
@@ -39,22 +32,10 @@ import { ChatComponent } from './noicefluid/chat/chat.component';
     BrowserModule,
     AppRoutingModule,
     AmplifyAuthenticatorModule,
-    MatSliderModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
     HttpClientModule,
-    MarkdownModule.forRoot({ loader: HttpClient }),
-    MatIconModule,
     MatRadioModule,
-    MatInputModule,
-    FormsModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatCardModule,
-    MatCardModule,
     YouTubePlayerModule,
-    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
